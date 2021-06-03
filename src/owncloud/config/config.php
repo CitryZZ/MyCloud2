8593bb77eb4a554d2c71b73f0ffa754798e3fe07
root <root@cnt8.lan>
2021-06-03 12:59:43 -0400

    v0.2

---
+++ src/owncloud/config/config.php
@@ -0,0 +1,17 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocvqwnxt37uq',
+  'passwordsalt' => '9a84d4gmhh1elJu0UV8TbqSfAymUy7',
+  'secret' => 'mr8yxDpsLitSa7T5E7+0KLDawH3UrEsbv+9Ouy+7lmKWYQDG',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.0.11:81',
+  ),
+  'datadirectory' => '/usr/share/nginx/html/owncloud/data',
+  'overwrite.cli.url' => 'http://192.168.0.11:81',
+  'dbtype' => 'mysql',
+  'version' => '10.7.0.4',
+  'dbname' => 'owncloud',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+);
